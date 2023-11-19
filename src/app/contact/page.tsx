8ccdf6b9665efb257ba7a77351f5b6e4bb20c8d4
Tintaa307 <101377457+Tintaa307@users.noreply.github.com>
2023-11-19 20:26:00 -0300

    change some margins

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -32,7 +32,7 @@ const Contact = () => {
   return (
     <main className="w-full h-screen flex items-center justify-center flex-col bg-primary_blue">
       <Toaster />
-      <div className="w-full h-[30%] flex items-center justify-center flex-row gap-8">
+      <div className="w-full h-[28%] flex items-center justify-center flex-row gap-8">
         <div className="w-[118px] h-[118px] bg-white rounded-sm flex items-center justify-center flex-col gap-2">
           <span className="w-16 h-16 bg-primary_blue rounded-full flex items-center justify-center">
             <IconPhone className="text-white font-bold" size={30} />
@@ -55,14 +55,14 @@ const Contact = () => {
         </Link>
       </div>
 
-      <div className="w-full h-[70%] flex items-center justify-start flex-col bg-white rounded-t-3xl">
-        <div className="w-full flex items-center justify-center h-max mt-8">
+      <div className="w-full h-[72%] flex items-center justify-start flex-col bg-white rounded-t-3xl">
+        <div className="w-full flex items-center justify-center h-max mt-6">
           <h2 className="text-black text-3xl font-bold">Contáctanos</h2>
         </div>
         <form
           onSubmit={handleSubmit}
           autoComplete="off"
-          className="w-full h-max flex items-center justify-center flex-col gap-6 mt-8"
+          className="w-full h-max flex items-center justify-center flex-col gap-6 mt-6"
         >
           <div className="w-full h-max flex items-center justify-start flex-col gap-1">
             <div className="w-full h-max flex items-start justify-start">
@@ -74,7 +74,7 @@ const Contact = () => {
               </label>
             </div>
             <input
-              className="w-[85%] h-14 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4"
+              className="w-[85%] h-14 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4 border-[1px] border-[#707070]"
               placeholder="Escriba su nombre"
               name="name"
               type="text"
@@ -90,7 +90,7 @@ const Contact = () => {
               </label>
             </div>
             <input
-              className="w-[85%] h-14 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4"
+              className="w-[85%] h-14 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4 border-[1px] border-[#707070]"
               placeholder="Escriba su email"
               name="email"
               type="email"
@@ -106,7 +106,7 @@ const Contact = () => {
               </label>
             </div>
             <textarea
-              className="w-[85%] h-24 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4 py-2"
+              className="w-[85%] h-24 border-none rounded-md outline-none shadow-[2px_4px_8px_#707070] text-black font-semibold px-4 py-2 border-[1px] border-[#707070]"
               placeholder="Escriba su mensaje"
               name="message"
               cols={30}
