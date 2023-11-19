b7e91716f1b9d8c4f762fa744f22440dbed37646
Tintaa307 <101377457+Tintaa307@users.noreply.github.com>
2023-11-19 15:25:56 -0300

    change menu

--- src/components/web/menu/Menu.tsx
+++ src/components/web/menu/Menu.tsx
@@ -17,7 +17,6 @@ import {
 const Menu = () => {
   const [selected, setSelected] = useState("/")
   const pathname = usePathname()
-  const [open, setOpen] = useState(true)
 
   // const handleSelected = (e: MouseEvent<HTMLLIElement>) => {
   //   const clicked = e.target as HTMLLIElement
@@ -108,42 +107,27 @@ const Menu = () => {
       {pathname !== "/login" && pathname !== "register" ? (
         <header
           className={cn(
-            "fixed bottom-0 left-0 w-full h-24 z-40 bg-white shadow-[0_0_10px_#00000040]",
+            "fixed bottom-0 left-0 w-full h-16 z-40 bg-white shadow-[0_0_10px_#00000040]",
             {
               "translate-y-0 transition-all duration-200": open,
               "translate-y-full transition-all duration-200": !open,
             }
           )}
         >
-          <div className="absolute w-full h-max flex items-center justify-center">
-            <div
-              onClick={() => setOpen(!open)}
-              className={cn(
-                "relative z-40 -top-10 w-14 h-14 bg-white rounded-full flex items-center justify-center",
-                {
-                  "-top-6": open,
-                }
-              )}
-            >
-              {open ? (
-                <IconChevronDown
-                  className="relative z-40"
-                  onClick={() => setOpen(true)}
-                />
-              ) : (
-                <IconChevronUp
-                  className="relative z-40"
-                  onClick={() => setOpen(false)}
-                />
-              )}
-            </div>
-          </div>
           <nav className="w-full h-full flex items-center justify-center">
             <ul className="w-full flex flex-row justify-between px-5 items-center">
               {navIcons.map((icon, index) => (
                 <div key={index}>
-                  <li onClick={() => setSelected(icon.url)} className="">
-                    <Link href={icon.url}>{icon.icon}</Link>
+                  <li
+                    onClick={() => setSelected(icon.url)}
+                    className="flex justify-center items-center gap-1 flex-col"
+                  >
+                    <Link className="mt-2" href={icon.url}>
+                      {icon.icon}
+                    </Link>
+                    <small className="text-black font-semibold">
+                      {icon.name}
+                    </small>
                   </li>
                 </div>
               ))}
