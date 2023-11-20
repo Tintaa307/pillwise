e2541f242e8ab9959027cd7d351b70dc0e5d5870
Tintaa307 <101377457+Tintaa307@users.noreply.github.com>
2023-11-20 00:38:35 -0300

    window hide

--- src/app/gps/page.tsx
+++ src/app/gps/page.tsx
@@ -8,16 +8,15 @@ import "leaflet/dist/leaflet.css"
 const Gps = () => {
   return (
     <>
-      {window !== undefined && (
-        <main className="w-full h-screen flex items-center justify-center">
-          <div className="w-full h-full flex items-center justify-start flex-col gap-12">
-            <div className="w-full h-max flex items-center justify-center">
-              <h1 className="text-black text-3xl font-semibold mt-12">
-                Busca tu pastillero
-              </h1>
-            </div>
-            <section className="relative w-[90%] h-[40%] border-[1.5px] border-black rounded-sm">
-              {/* <MapContainer
+      <main className="w-full h-screen flex items-center justify-center">
+        <div className="w-full h-full flex items-center justify-start flex-col gap-12">
+          <div className="w-full h-max flex items-center justify-center">
+            <h1 className="text-black text-3xl font-semibold mt-12">
+              Busca tu pastillero
+            </h1>
+          </div>
+          <section className="relative w-[90%] h-[40%] border-[1.5px] border-black rounded-sm">
+            {/* <MapContainer
                 className="w-full h-full"
                 center={[51.505, -0.09]}
                 zoom={13}
@@ -28,28 +27,27 @@ const Gps = () => {
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
               </MapContainer> */}
-            </section>
-            <div className="w-full h-max flex items-center justify-center flex-col gap-4">
-              <div>
-                <button className="w-[350px] h-[50px] bg-primary_blue text-white text-lg font-medium rounded-xl flex items-center justify-start">
-                  <IconReload className="absolute mx-4" />
-                  <span className="w-full flex items-center justify-center">
-                    Refrescar
-                  </span>
-                </button>
-              </div>
-              <div>
-                <button className="w-[350px] h-[50px] bg-primary_blue text-white text-lg font-medium rounded-xl flex items-center justify-start">
-                  <IconMusic className="absolute mx-4" />
-                  <span className="w-full flex items-center justify-center">
-                    Sonar
-                  </span>
-                </button>
-              </div>
+          </section>
+          <div className="w-full h-max flex items-center justify-center flex-col gap-4">
+            <div>
+              <button className="w-[350px] h-[50px] bg-primary_blue text-white text-lg font-medium rounded-xl flex items-center justify-start">
+                <IconReload className="absolute mx-4" />
+                <span className="w-full flex items-center justify-center">
+                  Refrescar
+                </span>
+              </button>
+            </div>
+            <div>
+              <button className="w-[350px] h-[50px] bg-primary_blue text-white text-lg font-medium rounded-xl flex items-center justify-start">
+                <IconMusic className="absolute mx-4" />
+                <span className="w-full flex items-center justify-center">
+                  Sonar
+                </span>
+              </button>
             </div>
           </div>
-        </main>
-      )}
+        </div>
+      </main>
     </>
   )
 }
