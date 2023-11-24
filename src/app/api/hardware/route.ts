95f6ea1779f378cbc2ffcf597b578afa0cf73229
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2023-11-24 10:32:03 -0300

    add req method

--- src/app/api/hardware/route.ts
+++ src/app/api/hardware/route.ts
@@ -3,6 +3,10 @@ import bcrypt from "bcrypt"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
+  if (req.method !== "POST") {
+    return NextResponse.json({ message: "Method not allowed" }, { status: 405 })
+  }
+
   try {
     const response = await req.json()
     console.log(response)
